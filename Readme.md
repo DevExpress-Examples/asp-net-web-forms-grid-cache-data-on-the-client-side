e895c609b313956d3dbe069d26ef566e534baac0
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# ASPxGridView - How to cache data on the client side
+
+
+<p>This example demonstrates how to cache data on the client to avoid unnecessary callbacks. In this example, data for two columns - "title_id" and "title" - is passed to the client and cached. Thus you can access this data later without invoking a callback.<br /><br /><strong>See Also:</strong><br /><a href="https://documentation.devexpress.com/#AspNet/CustomDocument11816">How to: Access Server Data on the Client Side</a> </p>
+
+<br/>
+
+
---
+++ VB/WebSite/JS/header.js
@@ -0,0 +1,40 @@
+﻿function _aspxTLoadStyleSheet(styleSheet) {
+    var link = document.createElement("link");
+    link.setAttribute("type", "text/css");
+    link.setAttribute("href", __aspxTutorialsBasePath + "/Css/" + styleSheet);
+    link.setAttribute("rel", "stylesheet");
+    var head = document.getElementsByTagName("head")[0];
+    head.appendChild(link);
+}
+function _aspxTSaveStateToCookies(isExpanded) {
+    document.cookie = "TutorialDescriptionExpanded=" + isExpanded.toString() + "; expires=Thu, 13 Sep 3007 14:07:07 GMT; path=/";
+}
+function _aspxTRemovePaddings() {
+    var descTd = document.getElementById('description');
+    if (descTd)
+        descTd.style.padding = "";
+}
+function _aspxTRestorePaddings() {
+    var descTd = document.getElementById('description');
+    if (descTd)
+        descTd.style.padding = "9px 11px 10px 11px";
+
+}
+function _aspxTExpandCollapseDescription() {
+    var desc = document.getElementById(__aspxTutorialHeaderClientID + "_pnlDescription");
+    var img = document.getElementById(__aspxTutorialHeaderClientID + "_imgExpandCollapse");
+    if (desc != null && img != null) {
+        if (desc.style.display == "none") {
+            img.src = img.src.replace("descArrowDown.gif", "descArrowUp.gif");
+            _aspxTSaveStateToCookies(true);
+            _aspxTRestorePaddings();
+            desc.style.display = "block";
+        } else {
+            img.src = img.src.replace("descArrowUp.gif", "descArrowDown.gif");
+            _aspxTSaveStateToCookies(false);
+            _aspxTRemovePaddings();
+            desc.style.display = "none";
+        }
+    }
+}
+_aspxTLoadStyleSheet("header.css");
